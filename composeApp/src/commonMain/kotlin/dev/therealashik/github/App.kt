1d31540784ca65f0383160d616cf3ce477697403
TheRealAshik <mashikahamed0@gmail.com>
2026-05-15 14:45:08 +0000

    fix: replace hardcoded black with M3 light/dark color schemes

--- composeApp/src/commonMain/kotlin/dev/therealashik/github/App.kt
+++ composeApp/src/commonMain/kotlin/dev/therealashik/github/App.kt
@@ -1,23 +1,24 @@
 package dev.therealashik.github
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
+
+private val LightColorScheme = lightColorScheme()
+private val DarkColorScheme = darkColorScheme()
 
 @Composable
 fun App() {
-    MaterialTheme(
-        colorScheme = darkColorScheme(
-            surface = Color.Black,
-            background = Color.Black
-        )
-    ) {
+    val colorScheme = if (isSystemInDarkTheme()) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(colorScheme = colorScheme) {
         var currentScreen by remember { mutableStateOf("splash") }
 
         Crossfade(targetState = currentScreen) { screen ->
